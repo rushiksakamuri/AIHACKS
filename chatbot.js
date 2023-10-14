c987f6bbd9d8b46e0309ddd0d8e8fdb3eab6c438
GriffinDale <116691203+GriffinDale@users.noreply.github.com>
2023-10-14 02:38:01 -0400

    Add files via upload

--- chatbot.js
+++ chatbot.js
@@ -1,11 +1,33 @@
-let name = document.getElementById('Name');
+let input = document.getElementById('input');
 let output = document.getElementById('Output');
 let homeButton = document.getElementById('HomeButton');
+let name = null;
+let annualSalary = -1;
 
-name.addEventListener('keydown', function (event) {
+input.addEventListener('keydown', function (event) {
     if (event.keyCode == 13) {
-        output.innerHTML = 'Hello ' + name.value;
-        console.log('Enter Pressed');
+        if (name == null) {
+            name = input.value;
+            output.innerHTML = output.innerHTML + '<br><br>' + input.value + '<br><br>' + 'Hello ' + input.value + ', what is your current annual salary in USD?';
+        }
+        else if (annualSalary == -1) {
+            console.log(Number(input.value) % 1 === 0);
+            if ((Number(input.value) % 1 === 0)) {
+                console.log(Number(input.value));
+                annualSalary = Number(input.value);
+                output.innerHTML = output.innerHTML + '<br><br>' + input.value + '<br><br>' + 'That means you make $' + ((annualSalary/12).toFixed(2)) +  ' a month!' ;
+            }
+            else {
+                output.innerHTML = output.innerHTML + '<br><br>' + input.value + '<br><br>' + 'Please enter only the number amount.';
+            }
+        }
+
+        else {
+            console.log('how did we get here');
+            //output.innerHTML = output.innerHTML + '<br><br>' + input.value + '<br><br';
+        }
+
+        document.getElementById('input').value = '';
     }
 });
 function redirect(page) {
